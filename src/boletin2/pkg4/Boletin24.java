cd662edfb963e39fa27ca370fa789848105ac4e4
rfernandezgonzalez <rfernandezgonzalez@eniac04.danielcastelao.org>
2015-01-26 09:17:44 +0100

    Commit inicial!

---
+++ src/boletin2/pkg4/Boletin24.java
@@ -0,0 +1,28 @@
+package boletin2.pkg4;
+
+import java.util.Scanner;
+
+public class Boletin24 {
+
+    public static void main(String[] args) {
+        
+        float num1 = 0;
+        float num2 = 0;
+
+        System.out.println("Introduzca el primer número para las operaciones:");
+        Scanner rec_num1 = new Scanner(System.in);
+
+        num1 = rec_num1.nextFloat();
+
+        System.out.println("Introduzca el segundo numero para operar:");
+        Scanner rec_num2 = new Scanner(System.in);
+
+        num2 = rec_num2.nextFloat();
+
+        System.out.println("La suma es: " + (num1 + num2));
+        System.out.println("La resta es: " + (num1 - num2));
+        System.out.println("El producto es: " + (num1 * num2));
+        System.out.println("El cociente es: " + (num1 / num2));
+    }
+
+}
